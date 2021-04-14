604f60b6fd6c40fc3f4543d6acc7835ddc5d700d
Mason Kinyon <mavhuntrx@gmail.com>
2021-04-14 08:10:40 -0700

    Initial commit.

--- org.rivervalley.EngagementScores/Model/EngagementScore.cs
+++ org.rivervalley.EngagementScores/Model/EngagementScore.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using Rock.Data;
+using Rock.Model;
 
 namespace org.rivervalley.EngagementScores.Model
 {
-    public class EngagementScore
+    [Table( "_org_rivervalley_EngagementScores" )]
+    [DataContract]
+    public class EngagementScore : Model<EngagementScore>, IRockEntity
     {
+        #region Entity Properties
+
+        [DataMember]
+        public DateTime SundayDate { get; set; }
+
+        [DataMember]
+        public int PersonAliasId { get; set; }
+
+        [DataMember]
+        public int Committing_NextCompletion { get; set; }
+
+        [DataMember]
+        public int Growing_WaterBaptism { get; set; }
+
+        #endregion
+
+        #region Virtual Properties
+
+        public virtual PersonAlias PersonAlias { get; set; }
+
+        #endregion  
+    }
+
+    public partial class EngagementScoreConfiguration : EntityTypeConfiguration<EngagementScore>
+    {
+        public EngagementScoreConfiguration()
+        {
+            this.HasRequired( r => r.PersonAlias ).WithMany().HasForeignKey( r => r.PersonAliasId ).WillCascadeOnDelete( false );
+
+            // IMPORTANT!!
+            this.HasEntitySetName( "EngagementScore" );
+        }
     }
 }
---
+++ org.rivervalley.EngagementScores/Model/EngagementScoreService.cs
@@ -0,0 +1,15 @@
+﻿using Rock.Data;
+
+namespace org.rivervalley.EngagementScores.Model
+{
+    public class EngagementScoreService : Service<EngagementScore>
+    {
+        public EngagementScoreService( RockContext context ) : base( context ) { }
+
+        public bool CanDelete( EngagementScore item, out string errorMessage )
+        {
+            errorMessage = string.Empty;
+            return true;
+        }
+    }
+}
